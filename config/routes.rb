c091025587fe9fb2a6915126ac808b61f06f1cda
Rodrigo Chavez <Rodrigo103004@gmail.com>
2024-05-13 15:30:28 -0600

    routes set up

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,16 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  namespace :api do
+    namespace :v0 do
+
+      resources :markets, only: [:index, :show] do 
+        resources :vendors, only: [:index]
+      end
+      resources :vendors, except: [:index]
+      resources :market_vendors, only: [:create, :destroy]
+
+    end
+  end
 end
