5584e4cb23f2f16af9c4c56802efe88c186fe1bb
Billy Wallace <wallace.billy1@gmail.com>
2024-05-16 16:10:03 -0600

    Add sad path test for nearest_atms_request

--- spec/requests/api/v0/nearest_atms_request_spec.rb
+++ spec/requests/api/v0/nearest_atms_request_spec.rb
@@ -38,8 +38,17 @@ describe "Nearest ATMs API" do
       end
     end
 
-    it "sad path" do
+    it "returns a 404 status and error message when an invalid market id is passed in", :vcr do
+      get "/api/v0/markets/123123123123123/nearest_atms"
 
+      expect(response).to_not be_successful
+      expect(response.status).to eq(404)
+
+      data = JSON.parse(response.body, symbolize_names: true)
+
+      expect(data[:errors]).to be_a(Array)
+      expect(data[:errors].first[:status]).to eq("404")
+      expect(data[:errors].first[:title]).to eq("Couldn't find Market with 'id'=123123123123123")
     end
   end
 end
