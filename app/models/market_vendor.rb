de7ef9f21edf4ce1e536fd124318e18318860553
Rodrigo Chavez <Rodrigo103004@gmail.com>
2024-05-17 01:14:49 -0600

    method if that id does not exist it will give an error either vendor or market

--- app/models/market_vendor.rb
+++ app/models/market_vendor.rb
@@ -1,4 +1,10 @@
 class MarketVendor < ApplicationRecord
   belongs_to :market
-  belongs_to :vendor 
+  belongs_to :vendor
+  validate :market_vendor_can_not_already_exist
+
+  def market_vendor_can_not_already_exist
+    existing_market_vendor = MarketVendor.find_by(market_id: market_id, vendor_id: vendor_id)
+    errors.add(:validation, "failed: Market vendor asociation between market with market_id=#{market_id} and vendor_id=#{vendor_id} already exists") if existing_market_vendor
+  end
 end
